1b0a83c902c35c6c5525db5e3e7bcbd6cb754f36
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-06-03 22:54:21 -0700

    Finish sign in proper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Authentication module SessionsHelper is an OOB Rails implementation
+  # Used extensively to manipulate Session objects over a stateless HTTP protocol
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
+
+    if @user.save                                 
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!" # Handle a successful save.
       redirect_to @user
     else
