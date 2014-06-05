66f3120d8479c6e72f8d48df4b3ee897635a494a
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-06-05 09:15:29 -0700

    Update users control actions access priveledge for edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+before_action :signed_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id]) 
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_parms)
+      #Handle successful here
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      rend 'edit'
+    end
+  end
   def show
   	@user = User.find(params[:id])
   end
@@ -24,5 +40,9 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user?
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
