5f242b47a5c142a06b7403e3eb0a337cb669df0d
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-06-05 13:49:00 -0700

    Add accessors to users controller actions with callback functions / fillers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 before_action :signed_in_user, only: [:edit, :update]
+before_action :correct_user, only: [:edit, :update]
 
   def new
   	@user = User.new
   end
 
   def edit
-    @user = User.find(params[:id]) 
   end
 
   def update
-    @user = User.find(params[:id])
+    # authenticatoin add so remove public update @user = User.find(params[:id])
     if @user.update_attributes(user_parms)
       #Handle successful here
       flash[:success] = "Profile updated"
@@ -19,6 +19,7 @@ before_action :signed_in_user, only: [:edit, :update]
       rend 'edit'
     end
   end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -42,7 +43,16 @@ before_action :signed_in_user, only: [:edit, :update]
                                    :password_confirmation)
     end
 
-    def signed_in_user?
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    def signed_in_user
+      #redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
     end
 end
