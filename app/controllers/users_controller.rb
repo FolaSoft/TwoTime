eecc7f4f3b78ba43b81ba22782021070db36a468
FloatingLegs <FolaDrives@FloatingLegs.com>
2014-06-13 22:42:22 -0700

    Micropost Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,13 +55,7 @@ before_action :admin_user, only: :destroy
                                    :password_confirmation)
     end
 
-    def signed_in_user
-      #redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  
 
     def correct_user
       @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
+  resources :microposts, only: [:create, :destroy]
+
   root  to: 'static_pages#home'
   match '/signin',      to: 'sessions#new',                  via: 'get'
   match '/signup',     to: 'users#new',                   via: 'get'
